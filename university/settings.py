0fac51a4c29061ee0c2d66dc1de140fe3b0b70ea
18031J0014 <44960336+18031J0014@users.noreply.github.com>
2019-06-13 21:01:54 +0530

    Update settings.py

--- university/settings.py
+++ university/settings.py
@@ -84,14 +84,8 @@ WSGI_APPLICATION = 'university.wsgi.application'
 
 DATABASES = {
     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'university',
-#         'USER': 'postgres',
-#         'PASSWORD': '1234',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
